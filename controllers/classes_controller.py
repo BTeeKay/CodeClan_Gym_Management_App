a23f3c2d4e939f81beef8da7f50c18be9e9e7d83
Brian Kerr <briantkerr91@gmail.com>
2022-05-30 18:09:14 +0100

    Can now add classes, todo: book member to a class

--- controllers/classes_controller.py
+++ controllers/classes_controller.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from models.classes import Classes
 
 import repositories.classes_repository as classes_repo
 import repositories.attend_repository as attend_repo
@@ -35,5 +36,14 @@ def edit_class(id):
 
 @classes_blueprint.route("/classes/add")
 def classes_add_view():
-    classes = classes_repo.select_all()
-    return render_template("members/add.html", title="Add Member", classes=classes)
+    return render_template("classes/add.html", title="Add Class")
+
+@classes_blueprint.route("/classes/add", methods=['POST'])
+def classes_add():
+    name = request.form['name']
+    capacity = request.form['capacity']
+    time = request.form['time']
+    time = time.replace("T", " ")
+    class1 = Classes(name, capacity, time)
+    classes_repo.save(class1)
+    return redirect('/classes')
